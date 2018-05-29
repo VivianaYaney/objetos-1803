12c9af73ded1f74343fbbcc9cf0b3d30a7987b22
T-101 <T-101@PC219>
2018-05-28 19:45:46 -0500

    contructores

---
+++ objetos-1803/src/contructores/ProbarConstructores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProbarConstructores {
+    public static void main(String[] args) {
+        //vamos a crear un fruta
+        Fruta f1=new Fruta();
+        
+        System.out.println("El nombre de la fruta es "+f1.nombre);
+        System.out.println("El precio de la fruta es "+f1.precio);
+        
+        float valor;
+        //System.out.println(valor);
+    
+    }
+}
