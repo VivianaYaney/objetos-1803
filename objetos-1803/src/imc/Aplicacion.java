6a3364b7e71053d7d5ad556f41ddd4bf4fa8239c
Viviana <Viviana@192.168.1.77>
2018-06-07 23:57:14 -0500

    IMC

--- objetos-1803/src/imc/Aplicacion.java
+++ objetos-1803/src/imc/Aplicacion.java
@@ -13,14 +13,23 @@ public class Aplicacion {
     public static void main(String[] args) {
         Usuario u=new Usuario();
         //A este objeto ñe asignamos un valor a su atributo centigrados
-        u.setAltura((float) 1.75);
-        u.setPeso(55);
+        u.setAltura((float) 1.80);
+        u.setPeso(80);
         //Creamos un objeto de la clase conversion 
         Imc i=new Imc();
         i.setUsuario(u);
         //aplicar el modelo
     
                 System.out.println(i.masa());
-        
+        if(i.masa()>=0 & i.masa()<20){
+            System.err.println("Bajo peso, posibilidad de anemia");
+        }
+        else if(i.masa()>=20 & i.masa()<25){
+            System.err.println("Peso ideal, normal");
+    }
+        else if(i.masa()>25){
+            System.err.println("Sobrepeso");
+    }
+
     }
 }
